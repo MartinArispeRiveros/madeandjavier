da9fb29ce84b26d06b9b2a93b4140c6b67f6f4d3
MartinArispeRiveros <martinarisperiveros@gmail.com>
2017-03-07 19:32:16 -0400

    Fisrt commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+// $to = "madelleine.mirabal.cano@gmail.com","hormi.ps@gmail.com";
+$to = "martinarisperiveros@gmail.com";
+$subject = "Contact from: " . $_POST["name"] . ' ' . $_POST["reason"];
+$message = 'Nombre: ' . $_POST["name"] . '<br />'
+        . $_POST["message"] . $_POST["message"];
+$header = "Reply-To:'" . $_POST["email"] . "'";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/html\r\n";
+$retval = mail($to, $subject, $message, $header);
+if ($retval == true) {
+    header('Content-Type: application/json');
+    echo json_encode(array('success' => TRUE));
+} else {
+    header('Content-Type: application/json');
+    echo json_encode(array('success' => FALSE));
+}
+?>
